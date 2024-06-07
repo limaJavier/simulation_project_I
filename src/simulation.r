27af15f17f56c66d2a2b8a6094710db1f70a7771
BritoAlv <gomzalezbrito@gmail.com>
2024-06-07 11:36:42 -0400

    explain more

--- src/simulation.r
+++ src/simulation.r
@@ -4,19 +4,20 @@ lambda <- 20 / 60 / 60 # seconds.
 miu <- 1 / 12 / 60  # seconds.
 iterations <- 60 * 60 * 1000  # seconds.
 
+queue_capacity <- 10
 
-states <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
+states <- seq(0, queue_capacity, 1)
 
 transition_matrix <- matrix(0, nrow = length(states), ncol = length(states))
 
-rate_one_arrival <- lambda * exp(-lambda)
-rate_one_departure <- miu * exp(-miu)
+rate_one_arrival <- lambda
+rate_one_departure <- miu 
 
 transition_matrix[1, 2] <- rate_one_arrival
 transition_matrix[1, 1] <- 1 - transition_matrix[1, 2]
 
-transition_matrix[11, 10] <- rate_one_departure
-transition_matrix[11, 11] <- 1 - transition_matrix[11, 10]
+transition_matrix[queue_capacity+1, queue_capacity] <- rate_one_departure
+transition_matrix[queue_capacity+1, queue_capacity+1] <- 1 - transition_matrix[queue_capacity+1, queue_capacity]
 
 for (i in 2:length(states) - 1) {
     for (j in 1:length(states)) {
@@ -36,44 +37,29 @@ for (i in 2:length(states) - 1) {
 
 mc <- new("markovchain", states = states, transitionMatrix = transition_matrix, name = "Car Washer", byrow = TRUE)
 
-results <- c()
-for (j in 1:200) {
+
+generate_results <- function(iteration)
+{
+  results <- c()
+  for (j in 1:200) {
     lose_client <- 0
     set.seed(24124*j + 2343234)
-    simulation <- rmarkovchain(iterations, mc, t0 = "0")
+    simulation <- rmarkovchain(iteration, mc, t0 = "0")
     for (i in 1:length(simulation))
     {
-        if (simulation[i] == "10") {
-            prob <- runif(1, 0, 1)
-            if (prob <= rate_one_arrival) {
-                lose_client <- lose_client + 1
-            }
+      if (simulation[i] == queue_capacity) {
+        prob <- runif(1, 0, 1)
+        if (prob <= rate_one_arrival) {
+          lose_client <- lose_client + 1
         }
+      }
     }
     results <- c(results, lose_client)
-    print(mean(results))
+  }
+  return(results)
 }
 
-# Assume that 'data' is your data
-data <- results # replace with your data
-
-# Calculate the mean and standard deviation
-mean_data <- mean(data)
-sd_data <- sd(data)
-
-
-# Create the histogram
-plot <- hist(data, main = "Histogram with Mean and SD", xlab = "Data", border = "black", col = "lightblue", xlim = range(c(data, mean_data - sd_data, mean_data + sd_data)))
-
-# Add the mean
-abline(v = mean_data, col = "red", lwd = 2)
-text(
-    x = mean_data, 
-    y = par("usr")[4], 
-    labels = paste("Mean =", round(mean_data, 2)), 
-    pos = 2, col = "red"
-)
-
-# Add the standard deviation
-abline(v = mean_data - sd_data, col = "blue", lwd = 2, lty = 2)
-abline(v = mean_data + sd_data, col = "blue", lwd = 2, lty = 2)
+vector_list <- lapply(iterations_range, generate_results)
+df <- as.data.frame(vector_list)
+colnames(df) <- c(iterations_range)
+write.csv(df, "./lost_average.csv")
